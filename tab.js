de8e548bda1231b618c31c6f5241f7ffa10be423
Wilson Tran <100881978+Wilson-Tran@users.noreply.github.com>
2022-04-16 01:51:08 -0700

    styled the page and included working  category selector

--- tab.js
+++ tab.js
@@ -1,7 +1,9 @@
-const giphy = {
+document.addEventListener('DOMContentLoaded', function() {
+  
+  const giphy = {
   baseURL: "https://api.giphy.com/v1/gifs/",
   apiKey: "0KVBqAG5lyQBSE7xLAPqyxSixw0OuFXF",
-  tag: "happy",
+  tag: "positivity",
   type: "random",
   rating: "pg-13"
 }; 
@@ -17,6 +19,19 @@ let giphyURL = encodeURI(
     giphy.rating
 );
 
+function createURL(tag) {
+  giphyURL = encodeURI(
+  giphy.baseURL +
+    giphy.type +
+    "?api_key=" +
+    giphy.apiKey +
+    "&tag=" +
+    tag +
+    "&rating=" +
+    giphy.rating
+);
+}
+
 function renderGif(giphyData) {
   console.log(giphyData);
   const gifWrap = document.getElementById("gif-wrap");
@@ -27,4 +42,45 @@ function renderGif(giphyData) {
 
 fetch(giphyURL)
   .then(response => response.json())
-  .then(data => renderGif(data));
+  .then(data => {renderGif(data);
+    console.log(data);});
+
+  const form = document.querySelector("form");
+  const submission = document.querySelector("#submit_button")
+  // submission.addEventListener('click', e => {
+  // e.preventDefault();
+  // const formData = new FormData(form);
+  // const value = formData.get()
+  // // console.log(formData);
+  // //giphy.tag = values[1];
+  // console.log(giphy.tag);
+  // }, false)
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    
+    for (const obj of formData) {
+      createURL(obj[1]);
+    }
+    fetch(giphyURL)
+    .then(response => response.json())
+    .then(data => {renderGif(data);
+    console.log(data);});
+
+  }
+})
+
+
+
+
+function selectChoice() {
+  var data = new FormData(form);
+  const formChoice = document.querySelector('category');
+  var output = "";
+  // for (const entry of data) {
+  //   output = output + entry[0] + "=" + entry[1] + "\r";
+  // };
+  // log.innerText = output;
+  event.preventDefault();
+  console.log(formChoice);
+}
